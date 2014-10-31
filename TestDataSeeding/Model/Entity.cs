7c7950630dfc82dc91277aab364be8e13762802b
pongracz-lajos <pongraczlajoslevente@gmail.com>
2014-11-01 00:41:36 +0200

    A projekt alapjai.

---
+++ TestDataSeeding/Model/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataSeeding.Model
+{
+    /// <summary>
+    /// Entity class, holds the values of the attributes defined in the corresponding EntityType.
+    /// </summary>
+    public class Entity
+    {
+    }
+}
