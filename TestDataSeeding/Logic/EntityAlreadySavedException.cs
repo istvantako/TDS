288a68bfe91e8a8969b537f243c380eb528da004
pongracz-lajos <pongraczlajoslevente@gmail.com>
2014-11-25 14:50:05 +0200

    Last commit repaired.

---
+++ TestDataSeeding/Logic/EntityAlreadySavedException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataSeeding.Logic
+{
+    /// <summary>
+    /// Test data seeding business logic exception, the entity has already been saved.
+    /// </summary>
+    public class EntityAlreadySavedException : TdsLogicException
+    {
+        public EntityAlreadySavedException()
+        {
+        }
+
+        public EntityAlreadySavedException(string message)
+            : base(message)
+        {
+        }
+
+        public EntityAlreadySavedException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ TestDataSeeding/Logic/TdsLogicException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataSeeding.Logic
+{
+    /// <summary>
+    /// Main Test Data Seeding business logic exception class.
+    /// </summary>
+    public class TdsLogicException : Exception
+    {
+        public TdsLogicException()
+        {
+        }
+
+        public TdsLogicException(string message)
+            : base(message)
+        {
+        }
+
+        public TdsLogicException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
