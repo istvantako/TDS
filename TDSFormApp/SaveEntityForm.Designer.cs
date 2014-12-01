ee8bbae5998502667350027410b199ca9146843a
renicy93 <renicy93@yahoo.com>
2014-12-02 00:33:21 +0200

    TDSFormApp-ban a comboboxok feltöltése és Label-Textbox párosok megjelenítése

--- TDSFormApp/SaveEntityForm.Designer.cs
+++ TDSFormApp/SaveEntityForm.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.entityCombobox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSize = true;
+            this.panel1.Location = new System.Drawing.Point(56, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(351, 327);
+            this.panel1.TabIndex = 5;
+            // 
+            // entityCombobox
+            // 
+            this.entityCombobox.FormattingEnabled = true;
+            this.entityCombobox.Location = new System.Drawing.Point(217, 30);
+            this.entityCombobox.Name = "entityCombobox";
+            this.entityCombobox.Size = new System.Drawing.Size(121, 21);
+            this.entityCombobox.TabIndex = 4;
+            this.entityCombobox.SelectedIndexChanged += new System.EventHandler(this.entityCombobox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(124, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Entity:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Enabled = false;
+            this.saveButton.Location = new System.Drawing.Point(366, 425);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
             // 
             // SaveEntityForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(470, 470);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.entityCombobox);
+            this.Controls.Add(this.label1);
             this.Name = "SaveEntityForm";
             this.Text = "Save Entity Form";
+            this.Load += new System.EventHandler(this.SaveEntityForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox entityCombobox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveButton;
     }
 }
 
--- TDSFormApp/SaveEntityForm.cs
+++ TDSFormApp/SaveEntityForm.cs
@@ -7,14 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TestDataSeeding.Logic;
+using TestDataSeeding.SerializedStorage;
+using TestDataSeeding.Model;
 
 namespace TDSFormApp
 {
     public partial class SaveEntityForm : Form
     {
+        //IStorage xml = new XmlStorageClient();
+        XmlStorageClient xml = new XmlStorageClient();
+        EntityStructures entityStructures = new EntityStructures();
+        EntityStructure entityStructure = new EntityStructure();
+
         public SaveEntityForm()
         {
             InitializeComponent();
+        }
+
+        private void SaveEntityForm_Load(object sender, EventArgs e)
+        {
+            entityStructures = xml.GetEntityStructures("d:\\TDS\\");
+            foreach (EntityStructure entity in entityStructures)
+            {
+                entityCombobox.Items.Add(entity.Name);
+            }
+        }
+
+        private void entityCombobox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.panel1.Controls.Clear();
+            entityStructure = entityStructures.Find(entityCombobox.SelectedItem.ToString());
+
+            int n = entityStructure.PrimaryKeys.Count;
+            Label[] pk_label = new Label[n];
+            TextBox[] pk_textbox = new TextBox[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                pk_label[i] = new Label();
+                pk_textbox[i] = new TextBox();
+            }
+            int k = 0;
+            foreach (String pk_name in entityStructure.PrimaryKeys)
+            {
+                pk_label[k].Text = pk_name;
+                pk_label[k].Name = pk_name + "Label: ";
+                pk_textbox[k].Name = pk_name + "Textbox";
+                k++;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                pk_label[i].Location = new Point(panel1.Left, panel1.Top + k);
+                this.panel1.Controls.Add(pk_label[i]);
+                //this.panel1.Controls.Add(new LiteralControl("<br />"));
+                pk_textbox[i].Location = new Point(panel1.Left + pk_label[i].Width, panel1.Top + k);
+                this.panel1.Controls.Add(pk_textbox[i]);
+                k += 30;
+            }
         }
     }
 }
