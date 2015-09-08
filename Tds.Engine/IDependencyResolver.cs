96346efde6f53c3335e428556d71838ca2da2413
pongracz-lajos <pongraczlajoslevente@gmail.com>
2015-09-08 18:01:26 +0300

    Added two interfaces, the first is for the maintenance tasks such as backup or restore, the second one is for resolving the dependencies between the entities.

---
+++ Tds.Engine/IDependencyResolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tds.Engine
+{
+    /// <summary>
+    /// This interface is implemented by any object that can resolve a dependency,
+    /// either directly or through use of an external container.
+    /// </summary>
+    interface IDependencyResolver
+    {
+    }
+}
---
+++ Tds.Engine/IMaintenanceTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tds.Engine
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    interface IMaintenanceTask
+    {
+        void Execute();
+    }
+}
